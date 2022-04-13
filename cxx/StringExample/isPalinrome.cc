bc4421d3b385d853f902e76132b450022a1ca881
sugar <787392786@qq.com>
2022-04-13 22:24:23 +0800

    isPalindrome.cc

---
+++ cxx/StringExample/isPalinrome.cc
@@ -0,0 +1,11 @@
+// @Author : Sugar 
+// @Created Time : 2022-04-13 22:12:36
+// @Description : 
+#include <string>
+#include <algorithm>
+
+bool isPalindrome(std::string &str){
+		std::string temp(str);
+	  std::reverse(temp.begin(), temp.end());
+		return str == temp;
+}
